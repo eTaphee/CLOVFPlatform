26dca445bdc25dfdc46787aa15b2449b53912028
eTaphee <43163409+eTaphee@users.noreply.github.com>
2023-05-31 20:32:58 +0900

    Employee 엔드포인트 추가
    컨트롤러, 모델

---
+++ CLOVFPlatform.Server/Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using CLOVFPlatform.Server.Models;
+
+namespace CLOVFPlatform.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly ILogger<EmployeeController> logger;
+
+        public EmployeeController(ILogger<EmployeeController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Employee>>> GetEmployeeAsync([FromQuery(Name = "page")] int page = 1, [FromQuery(Name = "pageSize")] int pageSize = 1)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{name}")]
+        public async Task<ActionResult<Employee>> GetEmployeeAsync(string name)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> PostEmployeeAsync()
+        {
+            return Created("", null);
+        }
+    }
+}
+
