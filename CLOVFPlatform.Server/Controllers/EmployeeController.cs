3349e89baca833b3b04889a13cc6bb1f68ff731a
eTaphee <43163409+eTaphee@users.noreply.github.com>
2023-06-03 23:30:22 +0900

    OpenAPI 를 이용해 API spec 노출 구현

--- CLOVFPlatform.Server/Controllers/EmployeeController.cs
+++ CLOVFPlatform.Server/Controllers/EmployeeController.cs
@@ -5,6 +5,9 @@ using EmployeeDTO = CLOVFPlatform.Server.Services.DTO.Employee;
 
 namespace CLOVFPlatform.Server.Controllers
 {
+    /// <summary>
+    /// employee controller
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class EmployeeController : ControllerBase
@@ -20,8 +23,16 @@ namespace CLOVFPlatform.Server.Controllers
             this.employeeService = employeeService;
         }
 
+        /// <summary>
+        /// 직원 목록 조회
+        /// </summary>
+        /// <param name="page">페이지 위치</param>
+        /// <param name="pageSize">페이지 항목 개수</param>
+        /// <returns>직원 목록</returns>
+        /// <response code="200">페이징 된 직원 목록 반환</response>
         [HttpGet]
         [Produces("application/json")]
+        [ProducesResponseType(typeof(PaginatedList<EmployeeDTO>), StatusCodes.Status200OK)]
         public async Task<ActionResult<PaginatedList<EmployeeDTO>>> GetEmployeeAsync([FromQuery(Name = "page")] int page = 1, [FromQuery(Name = "pageSize")] int pageSize = 5)
         {
             try
@@ -34,8 +45,17 @@ namespace CLOVFPlatform.Server.Controllers
             }
         }
 
+        /// <summary>
+        /// 이름으로 직원 정보 조회
+        /// </summary>
+        /// <param name="name">직원 이름</param>
+        /// <param name="page">페이지 위치</param>
+        /// <param name="pageSize">페이지 항목 개수</param>
+        /// <returns>이름으로 필터링된 직원 목록</returns>
+        /// <response code="200">페이징 된 직원 목록 반환</response>
         [HttpGet("{name}")]
         [Produces("application/json")]
+        [ProducesResponseType(typeof(PaginatedList<EmployeeDTO>), StatusCodes.Status200OK)]
         public async Task<ActionResult> GetEmployeeAsync(string name, [FromQuery(Name = "page")] int page = 1, [FromQuery(Name = "pageSize")] int pageSize = 5)
         {
             try
@@ -48,9 +68,19 @@ namespace CLOVFPlatform.Server.Controllers
             }
         }
 
+        /// <summary>
+        /// 직원 정보 입력
+        /// </summary>
+        /// <param name="file">직원 정보 파일(json, csv)</param>
+        /// <returns>추가된 직원 정보</returns>
+        /// <exception cref="CLOVFPlatformException"></exception>
+        /// <response code="201">페이징 된 직원 목록 반환(여러명인 경우 배열)</response>
+        /// <response code="400">입력 매개변수에 문제가 있을 때</response>
         [HttpPost]
         [Consumes("multipart/form-data", "application/json", "text/csv")]
         [Produces("application/json")]
+        [ProducesResponseType(typeof(EmployeeDTO), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ErrorMessage), StatusCodes.Status400BadRequest)]
         public async Task<ActionResult> PostEmployeeAsync(IFormFile? file)
         {
             try
--- CLOVFPlatform.Server/Program.cs
+++ CLOVFPlatform.Server/Program.cs
@@ -2,6 +2,8 @@
 using CLOVFPlatform.Server.Models;
 using CLOVFPlatform.Server.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,7 +21,30 @@ builder.Services.AddRouting((options) => { options.LowercaseUrls = true; });
 builder.Services.AddControllers().AddNewtonsoftJson((options) => { options.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;  });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen((options) =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "CLO VF Platform API",
+        Description = "An ASP.NET Core Web API for clovf employee",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }
+    });
+
+    // using System.Reflection;
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 builder.Services.AddSwaggerGenNewtonsoftSupport(); // swagger newtonsoft.json 
 
 builder.Services.AddDbContext<CLOVFContext>();
